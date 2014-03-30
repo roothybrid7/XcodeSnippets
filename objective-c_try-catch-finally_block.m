3aaf5e6d619c4326f0498d912c476277985c69c5
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 12:02:49 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- objective-c_try-catch-finally_block.m
+++ objective-c_try-catch-finally_block.m
@@ -3,11 +3,11 @@
 //
 // IDECodeSnippetCompletionPrefix: @try
 // IDECodeSnippetCompletionScopes: [CodeBlock]
-// IDECodeSnippetIdentifier: 5A500F6D-22F9-415A-BFD2-AC633A2B696F
+// IDECodeSnippetIdentifier: 86A06382-CB03-4049-9191-79692D23E69E
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetRelativePriority: 1
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 @try {
     <#Code that can potentially throw an exception#>
 }
