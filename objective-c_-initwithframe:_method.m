ce4971b99c03870ca6e072f3c21930a14f3b4a42
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 12:05:21 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- objective-c_-initwithframe:_method.m
+++ objective-c_-initwithframe:_method.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: initWithFrame
 // IDECodeSnippetCompletionScopes: [ClassImplementation]
-// IDECodeSnippetIdentifier: 807AC058-0340-4358-A2AB-246576154DC9
+// IDECodeSnippetIdentifier: 2C50463E-7CDF-4590-B32F-64755824CFF1
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 - (instancetype)initWithFrame:(NSRect)frame
 {
     self = [super initWithFrame:frame];
