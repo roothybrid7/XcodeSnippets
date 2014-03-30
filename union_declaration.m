3ae09c7f1de603d5516d526ffbabc5af4d2a7615
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 12:03:32 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- union_declaration.m
+++ union_declaration.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: uniondef
 // IDECodeSnippetCompletionScopes: [CodeBlock,TopLevel]
-// IDECodeSnippetIdentifier: E1953353-0E0A-4807-80B8-AFAD5F6B0A21
+// IDECodeSnippetIdentifier: 0D45EC0C-0939-4F30-81C3-092078BFD091
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 union <#union name#> {
   <#union fields#>
 };
