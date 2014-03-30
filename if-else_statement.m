5710e1d793303aa86870ced75b8d1da2aabda371
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 12:06:06 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- if-else_statement.m
+++ if-else_statement.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: ifelse
 // IDECodeSnippetCompletionScopes: [CodeBlock]
-// IDECodeSnippetIdentifier: 324A989C-B8B7-4974-8F12-D4B42F2059BA
+// IDECodeSnippetIdentifier: 52E8C99A-8B00-456C-90D4-CD65A4541D44
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 if (<#condition#>) {
     <#statements#>
 } else {
