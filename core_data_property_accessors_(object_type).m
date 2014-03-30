6f8a4dbe6a43496c6ba72f6666c45e1ebae9dce4
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 12:02:44 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- core_data_property_accessors_(object_type).m
+++ core_data_property_accessors_(object_type).m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: 
 // IDECodeSnippetCompletionScopes: [ClassImplementation]
-// IDECodeSnippetIdentifier: 5DA67A34-6770-496E-8043-67FBA237CA5D
+// IDECodeSnippetIdentifier: 58BEC783-1C0E-42FA-915D-684B2178C7D3
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 - (<#propertyObjectType#> *)<#propertyName#>
 {
     [self willAccessValueForKey:@"<#propertyName#>"];
