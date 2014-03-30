f49ccc64ad665f71af4a7daab39acc68c644b834
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 12:06:00 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- objective-c_category.m
+++ objective-c_category.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: @interface-category
 // IDECodeSnippetCompletionScopes: [TopLevel]
-// IDECodeSnippetIdentifier: 4E041FB2-AB0D-4249-9658-141289641F6B
+// IDECodeSnippetIdentifier: FFA53A14-91B2-45A6-A2B0-7EECD56976D6
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 @interface <#class name#> (<#category name#>)
 
 @end
