9112766f18e106b3485bcfed1f116afe104ebc08
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 12:04:39 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- struct_declaration.m
+++ struct_declaration.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: structdef
 // IDECodeSnippetCompletionScopes: [CodeBlock,TopLevel]
-// IDECodeSnippetIdentifier: 41F64F7F-EFC8-4CDE-B129-EBC903B10F3F
+// IDECodeSnippetIdentifier: 03909CDA-1E2F-46DD-8C3D-3A68BDDD2AAC
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 struct <#struct name#> {
   <#struct fields#>
 };
