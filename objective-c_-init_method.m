0689ee12fcee2b51d2cf6ebc057fbb7a9583602e
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 12:04:45 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- objective-c_-init_method.m
+++ objective-c_-init_method.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: init
 // IDECodeSnippetCompletionScopes: [ClassImplementation]
-// IDECodeSnippetIdentifier: B25FEFF4-89B8-4266-85F4-1DC0CB2B6769
+// IDECodeSnippetIdentifier: DFF5988C-C559-4F06-A097-C07F877DCFAD
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 - (instancetype)init
 {
     self = [super init];
