44592caaf592c7fea1b52ad6771644f8629b183d
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 12:02:58 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- objective-c_fast_enumeration.m
+++ objective-c_fast_enumeration.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: forin
 // IDECodeSnippetCompletionScopes: [CodeBlock]
-// IDECodeSnippetIdentifier: 60CB781B-02C5-4C32-B205-44EEF8209DBC
+// IDECodeSnippetIdentifier: 3A53E88B-30E4-4666-B889-A2E3AD5A5A3D
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 for (<#type *object#> in <#collection#>) {
     <#statements#>
 }
