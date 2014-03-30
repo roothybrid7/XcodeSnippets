b6714ce46bd52b97bf4e8c3311c5d7fd9dc36363
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 12:02:54 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- objective-c_-compare:_method.m
+++ objective-c_-compare:_method.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: compare
 // IDECodeSnippetCompletionScopes: [ClassImplementation]
-// IDECodeSnippetIdentifier: 89FE0BA9-EBF2-4D4E-A5E1-F09592F3664F
+// IDECodeSnippetIdentifier: A9CF3F95-0C35-4CFC-9D27-72EF4530848C
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 - (NSComparisonResult)compare:(id)other
 {
     return <#comparison expression#>;
