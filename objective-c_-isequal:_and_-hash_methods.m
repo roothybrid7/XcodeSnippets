09916d8ee23d7725d6421c652bcb46f7cfca4c45
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 12:03:16 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- objective-c_-isequal:_and_-hash_methods.m
+++ objective-c_-isequal:_and_-hash_methods.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: isEqual
 // IDECodeSnippetCompletionScopes: [ClassImplementation]
-// IDECodeSnippetIdentifier: 30A07915-4F35-485B-902C-F416C822B759
+// IDECodeSnippetIdentifier: 82ABE4D3-B806-4DAE-8FE9-4BD756B2CF39
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 - (BOOL)isEqual:(id)other
 {
     if (other == self) {
