f31838fd9484be49c0a7b9597b32f1b2ff53c72e
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 12:04:26 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- for_statement.m
+++ for_statement.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: for
 // IDECodeSnippetCompletionScopes: [CodeBlock]
-// IDECodeSnippetIdentifier: 167AF381-F451-4535-900D-D2B65C99DB1F
+// IDECodeSnippetIdentifier: 922F20CF-E909-4787-B15B-5558BB9E5FD7
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 for (<#initialization#>; <#condition#>; <#increment#>) {
     <#statements#>
 }
