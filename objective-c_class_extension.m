d615d9e8fe4c3d206375efcbf29c82a86f485284
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 12:03:26 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- objective-c_class_extension.m
+++ objective-c_class_extension.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: @interface-extension
 // IDECodeSnippetCompletionScopes: [TopLevel]
-// IDECodeSnippetIdentifier: DE1BFE54-6A56-4424-9DDC-C4ACBBC649B2
+// IDECodeSnippetIdentifier: 8414F4C7-2303-4115-AED3-15D26D1C098A
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 @interface <#class name#> ()
 
 @end
