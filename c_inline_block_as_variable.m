bf2b94f7537aefac70f9b00d37fe3683da470262
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 12:02:40 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- c_inline_block_as_variable.m
+++ c_inline_block_as_variable.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: inlineBlock
 // IDECodeSnippetCompletionScopes: [CodeBlock]
-// IDECodeSnippetIdentifier: 83CBFBD3-CDC7-4637-8CCE-FEE243AF09AE
+// IDECodeSnippetIdentifier: E7B7F708-E83B-475F-9A23-C72F5FF49B5E
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 <#returnType#>(^<#blockName#>)(<#parameterTypes#>) = ^(<#parameters#>) {
     <#statements#>
 };
