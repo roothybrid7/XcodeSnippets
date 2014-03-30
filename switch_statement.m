38c97f59ae6aa9ba64dbfaa6e6700057b89e4b9b
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 12:04:30 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- switch_statement.m
+++ switch_statement.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: switch
 // IDECodeSnippetCompletionScopes: [CodeBlock]
-// IDECodeSnippetIdentifier: DA95C97A-3F49-4A24-AFF7-8ACE73B0C6FA
+// IDECodeSnippetIdentifier: 61D2F483-9376-476E-A3A5-FDA8BFA6115C
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 switch (<#expression#>) {
   case <#constant#>:
     <#statements#>
