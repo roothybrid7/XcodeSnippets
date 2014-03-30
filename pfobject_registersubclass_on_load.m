50a58655407af5884c5beece11eaa32cc26bbace
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 11:56:26 +0900

    Imported user code snippets

---
+++ pfobject_registersubclass_on_load.m
@@ -0,0 +1,14 @@
+// PFObject registerSubclass on load
+// 
+//
+// IDECodeSnippetCompletionScopes: [CodeExpression]
+// IDECodeSnippetIdentifier: 92BDE388-2ADB-4072-93EF-6F0775FEBDE9
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+
+    __weak typeof(self) weakSelf = self;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        [weakSelf registerSubclass];
+    });
