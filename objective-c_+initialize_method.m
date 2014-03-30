8fcd7dffef49965d55c8d7d208bdd599d3ded3e7
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 12:03:36 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- objective-c_+initialize_method.m
+++ objective-c_+initialize_method.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: initialize
 // IDECodeSnippetCompletionScopes: [ClassImplementation]
-// IDECodeSnippetIdentifier: F9022097-FD7D-45DC-A790-A2EA6845386F
+// IDECodeSnippetIdentifier: 53D528E7-CC1D-4C35-AF67-483276408C78
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 + (void)initialize
 {
     if (self == [<#ClassName#> class]) {
