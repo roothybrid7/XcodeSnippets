58e105fde76f14dc0d84ac75996be19e27b3152a
Satoshi Ohki <roothybrid7@gmail.com>
2014-03-30 12:05:46 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- c++_using_directive.m
+++ c++_using_directive.m
@@ -3,8 +3,8 @@
 //
 // IDECodeSnippetCompletionPrefix: using namespace
 // IDECodeSnippetCompletionScopes: [CodeBlock,TopLevel]
-// IDECodeSnippetIdentifier: 5BEE2080-03BF-4B7A-989F-7507C8364503
+// IDECodeSnippetIdentifier: 729758FC-857F-48D2-955C-4E35911F4ED1
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.C-Plus-Plus
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 using namespace <#namespace name#>
