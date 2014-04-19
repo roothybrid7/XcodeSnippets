8b0a3aaeb1e0ab4536c330c74ba6fc9561352414
Satoshi Ohki <roothybrid7@gmail.com>
2014-04-19 12:03:51 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- ignored_objc_protocol_method_implementation.m
+++ ignored_objc_protocol_method_implementation.m
@@ -1,6 +1,7 @@
 // Ignored ObjC protocol method implementation
 // 
 //
+// IDECodeSnippetCompletionPrefix: ignoredobjcprotooverride
 // IDECodeSnippetCompletionScopes: [ClassImplementation]
 // IDECodeSnippetIdentifier: B086D1CE-4B54-4629-A475-CA6296670824
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
