7a760ee42f2021d72cd709f7f290d43f2dbc49c1
Satoshi Ohki <roothybrid7@gmail.com>
2014-04-20 15:32:19 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- pragma_-_ignored_arc_performselector_leaks.m
+++ pragma_-_ignored_arc_performselector_leaks.m
@@ -6,7 +6,7 @@
 // IDECodeSnippetIdentifier: FFFDDC2A-FB00-4944-96AD-4FD4C569C563
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 0
+// IDECodeSnippetVersion: 2
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
     <#statements#>
