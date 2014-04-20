cb32db515e7465a53063dc79eae7ec5c48773c0b
Satoshi Ohki <roothybrid7@gmail.com>
2014-04-20 15:32:11 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- ignored_arc_retain_cycles.m
+++ ignored_arc_retain_cycles.m
@@ -6,7 +6,7 @@
 // IDECodeSnippetIdentifier: 6B5D176F-D892-449C-9F15-6588D27076D5
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 0
+// IDECodeSnippetVersion: 2
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Warc-retain-cycles"
     <#statements#>
