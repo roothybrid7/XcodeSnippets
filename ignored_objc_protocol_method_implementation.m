d04b02f187ce47762ac76f03f45d0486cfcb9292
Satoshi Ohki <roothybrid7@gmail.com>
2014-04-20 15:32:15 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- ignored_objc_protocol_method_implementation.m
+++ ignored_objc_protocol_method_implementation.m
@@ -6,7 +6,7 @@
 // IDECodeSnippetIdentifier: B086D1CE-4B54-4629-A475-CA6296670824
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 0
+// IDECodeSnippetVersion: 2
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wobjc-protocol-method-implementation"
 <#scope#> (<#ReturnType#>)<#MethodSignature#>
