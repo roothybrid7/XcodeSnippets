0109f0253484a108e0734dbd24f05dc77db07d28
Satoshi Ohki <roothybrid7@gmail.com>
2014-08-05 11:49:03 +0900

    Subject line.
    Description here.
    
    [ticket: X]

---
+++ pragma_-_ignored_warc_retain_cycles.m
@@ -0,0 +1,12 @@
+// Pragma - Ignored Warc retain cycles
+// 
+//
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 2DD11C4C-BA62-42D1-83E4-276D2AF98327
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 0
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-retain-cycles"
+        textSize = [text sizeWithFont:font constrainedToSize:CGSizeMake(label.frame.size.width, CGFLOAT_MAX) lineBreakMode:NSLineBreakByWordWrapping];
+#pragma clang diagnostic pop
