069a6b17b3c64d08d7b655c2212cf3c57d3b2c6b
Satoshi Ohki <roothybrid7@gmail.com>
2014-08-09 10:05:08 +0900

    Subject line.
    Description here.
    
    [ticket: X]

---
+++ gcd:_dispatch_after.m
@@ -0,0 +1,12 @@
+// gcd:_dispatch_after
+// Execute a block after a set amount of time.
+//
+// IDECodeSnippetCompletionPrefix: dispatch_after snippet
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: CD061051-0A27-44AD-A65D-9082275063E6
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(<#delayInSeconds#> * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    <#code to be executed after a specified delay#>
+});
