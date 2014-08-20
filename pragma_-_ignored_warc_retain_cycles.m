08136b0bdad6b153744ea2eaa630bd094a299a91
Satoshi Ohki <roothybrid7@gmail.com>
2014-08-20 18:17:03 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- pragma_-_ignored_warc_retain_cycles.m
+++ pragma_-_ignored_warc_retain_cycles.m
@@ -6,7 +6,7 @@
 // IDECodeSnippetIdentifier: 2DD11C4C-BA62-42D1-83E4-276D2AF98327
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 0
+// IDECodeSnippetVersion: 2
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Warc-retain-cycles"
     <#statement#>
