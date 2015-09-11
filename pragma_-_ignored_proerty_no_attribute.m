cad7d4cf27a2b8fe26a5ce40099aaa9bf1d2b578
Satoshi Ohki <roothybrid7@gmail.com>
2015-09-11 23:35:53 +0900

    Subject line.
    Description here.
    
    [ticket: X]

--- pragma_-_ignored_proerty_no_attribute.m
+++ pragma_-_ignored_proerty_no_attribute.m
@@ -1,11 +1,11 @@
 // Pragma - Ignored proerty no attribute
-// 
+// Suppress warning for the property of RLMObject
 //
 // IDECodeSnippetCompletionScopes: [ClassInterfaceMethods]
 // IDECodeSnippetIdentifier: 8E9C6CE3-D045-4B83-8371-246532F55E83
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C-Plus-Plus
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 0
+// IDECodeSnippetVersion: 2
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wobjc-property-no-attribute"
 
