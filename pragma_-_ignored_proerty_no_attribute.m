527b9b02834bdb8de0dfd20f4dc4cf12d82545c5
Satoshi Ohki <roothybrid7@gmail.com>
2015-09-11 23:34:27 +0900

    Subject line.
    Description here.
    
    [ticket: X]

---
+++ pragma_-_ignored_proerty_no_attribute.m
@@ -0,0 +1,28 @@
+// Pragma - Ignored proerty no attribute
+// 
+//
+// IDECodeSnippetCompletionScopes: [ClassInterfaceMethods]
+// IDECodeSnippetIdentifier: 8E9C6CE3-D045-4B83-8371-246532F55E83
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C-Plus-Plus
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 0
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wobjc-property-no-attribute"
+
+@property NSString *objectID;
+@property NSString *organizationMembershipID;
+
+@property NSString *organizationID;
+@property NSDate *createdAt;
+@property NSDate *updatedAt;
+@property NSDate *deletedAt;
+
+@property NSInteger lockVersion;
+
+#pragma mark RelationShips
+
+@property GPTRLMUser *user;
+@property GPTRLMOrganization *organization;
+@property GPTRLMRole *role;
+
+#pragma clang diagnostic pop
